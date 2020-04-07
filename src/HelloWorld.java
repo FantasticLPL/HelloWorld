f40e133b5f041750fa4b5a69a43d032613c73fe2
FantasticLPL <lpl8898@foxmail.com>
2020-04-07 16:27:14 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi,everyone! Hello,this world!");
+        System.out.println("Hi,everyone! Hello,this world! Hi!");
     }
 }
